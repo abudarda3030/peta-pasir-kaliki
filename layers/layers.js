15328fba01a2af9991420d5ef3334973b51de310
abudarda3030 <mabu@student.poltekkesbandung.ac.id>
2024-09-12 17:57:18 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,98 @@
+var wms_layers = [];
+
+var format_kelurahanpasirkalikii_0 = new ol.format.GeoJSON();
+var features_kelurahanpasirkalikii_0 = format_kelurahanpasirkalikii_0.readFeatures(json_kelurahanpasirkalikii_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_kelurahanpasirkalikii_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_kelurahanpasirkalikii_0.addFeatures(features_kelurahanpasirkalikii_0);
+var lyr_kelurahanpasirkalikii_0 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_kelurahanpasirkalikii_0, 
+                style: style_kelurahanpasirkalikii_0,
+                popuplayertitle: "kelurahan pasirkalikii",
+                interactive: true,
+                title: '<img src="styles/legend/kelurahanpasirkalikii_0.png" /> kelurahan pasirkalikii'
+            });
+var format_titikvektor_1 = new ol.format.GeoJSON();
+var features_titikvektor_1 = format_titikvektor_1.readFeatures(json_titikvektor_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_titikvektor_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_titikvektor_1.addFeatures(features_titikvektor_1);
+var lyr_titikvektor_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_titikvektor_1, 
+                style: style_titikvektor_1,
+                popuplayertitle: "titik vektor",
+                interactive: true,
+                title: '<img src="styles/legend/titikvektor_1.png" /> titik vektor'
+            });
+var format_buffer_2 = new ol.format.GeoJSON();
+var features_buffer_2 = format_buffer_2.readFeatures(json_buffer_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_buffer_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_buffer_2.addFeatures(features_buffer_2);
+var lyr_buffer_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_buffer_2, 
+                style: style_buffer_2,
+                popuplayertitle: "buffer",
+                interactive: true,
+                title: '<img src="styles/legend/buffer_2.png" /> buffer'
+            });
+var format_JALAN_LN_25K_3 = new ol.format.GeoJSON();
+var features_JALAN_LN_25K_3 = format_JALAN_LN_25K_3.readFeatures(json_JALAN_LN_25K_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_JALAN_LN_25K_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_JALAN_LN_25K_3.addFeatures(features_JALAN_LN_25K_3);
+var lyr_JALAN_LN_25K_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_JALAN_LN_25K_3, 
+                style: style_JALAN_LN_25K_3,
+                popuplayertitle: "JALAN_LN_25K",
+                interactive: true,
+                title: '<img src="styles/legend/JALAN_LN_25K_3.png" /> JALAN_LN_25K'
+            });
+var format_SUNGAI_LN_25K_4 = new ol.format.GeoJSON();
+var features_SUNGAI_LN_25K_4 = format_SUNGAI_LN_25K_4.readFeatures(json_SUNGAI_LN_25K_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_SUNGAI_LN_25K_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_SUNGAI_LN_25K_4.addFeatures(features_SUNGAI_LN_25K_4);
+var lyr_SUNGAI_LN_25K_4 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_SUNGAI_LN_25K_4, 
+                style: style_SUNGAI_LN_25K_4,
+                popuplayertitle: "SUNGAI_LN_25K",
+                interactive: true,
+                title: '<img src="styles/legend/SUNGAI_LN_25K_4.png" /> SUNGAI_LN_25K'
+            });
+
+lyr_kelurahanpasirkalikii_0.setVisible(true);lyr_titikvektor_1.setVisible(true);lyr_buffer_2.setVisible(true);lyr_JALAN_LN_25K_3.setVisible(true);lyr_SUNGAI_LN_25K_4.setVisible(true);
+var layersList = [lyr_kelurahanpasirkalikii_0,lyr_titikvektor_1,lyr_buffer_2,lyr_JALAN_LN_25K_3,lyr_SUNGAI_LN_25K_4];
+lyr_kelurahanpasirkalikii_0.set('fieldAliases', {'AREA': 'AREA', 'PERIMETER': 'PERIMETER', 'CIMAHI_ADM': 'CIMAHI_ADM', 'CIMAHI_A_1': 'CIMAHI_A_1', 'KECAMATAN': 'KECAMATAN', 'KELURAHAN': 'KELURAHAN', 'RW': 'RW', 'RT': 'RT', });
+lyr_titikvektor_1.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_buffer_2.set('fieldAliases', {'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_JALAN_LN_25K_3.set('fieldAliases', {'NAMRJL': 'NAMRJL', 'KONRJL': 'KONRJL', 'MATRJL': 'MATRJL', 'FGSRJL': 'FGSRJL', 'UTKRJL': 'UTKRJL', 'TOLRJL': 'TOLRJL', 'WLYRJL': 'WLYRJL', 'AUTRJL': 'AUTRJL', 'KLSRJL': 'KLSRJL', 'SPCRJL': 'SPCRJL', 'JPARJL': 'JPARJL', 'ARHRJL': 'ARHRJL', 'STARJL': 'STARJL', 'KLLRJL': 'KLLRJL', 'MEDRJL': 'MEDRJL', 'LOCRJL': 'LOCRJL', 'JARRJL': 'JARRJL', 'FCODE': 'FCODE', 'REMARK': 'REMARK', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'SHAPE_Leng': 'SHAPE_Leng', });
+lyr_SUNGAI_LN_25K_4.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'TIPSNG': 'TIPSNG', 'KLSSNG': 'KLSSNG', 'FCODE': 'FCODE', 'REMARK': 'REMARK', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'NAMWS': 'NAMWS', 'NAMDAS': 'NAMDAS', 'STATUS': 'STATUS', 'WMAX': 'WMAX', 'DBTMAX': 'DBTMAX', 'SLPRT': 'SLPRT', 'SHAPE_Leng': 'SHAPE_Leng', });
+lyr_kelurahanpasirkalikii_0.set('fieldImages', {'AREA': 'TextEdit', 'PERIMETER': 'TextEdit', 'CIMAHI_ADM': 'TextEdit', 'CIMAHI_A_1': 'TextEdit', 'KECAMATAN': 'TextEdit', 'KELURAHAN': 'TextEdit', 'RW': 'TextEdit', 'RT': 'TextEdit', });
+lyr_titikvektor_1.set('fieldImages', {'Name': '', 'descriptio': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMo': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_buffer_2.set('fieldImages', {'Name': 'TextEdit', 'descriptio': 'TextEdit', 'timestamp': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'altitudeMo': 'TextEdit', 'tessellate': 'TextEdit', 'extrude': 'TextEdit', 'visibility': 'TextEdit', 'drawOrder': 'TextEdit', 'icon': 'TextEdit', });
+lyr_JALAN_LN_25K_3.set('fieldImages', {'NAMRJL': '', 'KONRJL': '', 'MATRJL': '', 'FGSRJL': '', 'UTKRJL': '', 'TOLRJL': '', 'WLYRJL': '', 'AUTRJL': '', 'KLSRJL': '', 'SPCRJL': '', 'JPARJL': '', 'ARHRJL': '', 'STARJL': '', 'KLLRJL': '', 'MEDRJL': '', 'LOCRJL': '', 'JARRJL': '', 'FCODE': '', 'REMARK': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'SHAPE_Leng': '', });
+lyr_SUNGAI_LN_25K_4.set('fieldImages', {'NAMOBJ': '', 'TIPSNG': '', 'KLSSNG': '', 'FCODE': '', 'REMARK': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'NAMWS': '', 'NAMDAS': '', 'STATUS': '', 'WMAX': '', 'DBTMAX': '', 'SLPRT': '', 'SHAPE_Leng': '', });
+lyr_kelurahanpasirkalikii_0.set('fieldLabels', {'AREA': 'no label', 'PERIMETER': 'no label', 'CIMAHI_ADM': 'no label', 'CIMAHI_A_1': 'no label', 'KECAMATAN': 'no label', 'KELURAHAN': 'no label', 'RW': 'no label', 'RT': 'no label', });
+lyr_titikvektor_1.set('fieldLabels', {'Name': 'no label', 'descriptio': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_buffer_2.set('fieldLabels', {'Name': 'no label', 'descriptio': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMo': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_JALAN_LN_25K_3.set('fieldLabels', {'NAMRJL': 'no label', 'KONRJL': 'no label', 'MATRJL': 'no label', 'FGSRJL': 'no label', 'UTKRJL': 'no label', 'TOLRJL': 'no label', 'WLYRJL': 'no label', 'AUTRJL': 'no label', 'KLSRJL': 'no label', 'SPCRJL': 'no label', 'JPARJL': 'no label', 'ARHRJL': 'no label', 'STARJL': 'no label', 'KLLRJL': 'no label', 'MEDRJL': 'no label', 'LOCRJL': 'no label', 'JARRJL': 'no label', 'FCODE': 'no label', 'REMARK': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'SHAPE_Leng': 'no label', });
+lyr_SUNGAI_LN_25K_4.set('fieldLabels', {'NAMOBJ': 'no label', 'TIPSNG': 'no label', 'KLSSNG': 'no label', 'FCODE': 'no label', 'REMARK': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'NAMWS': 'no label', 'NAMDAS': 'no label', 'STATUS': 'no label', 'WMAX': 'no label', 'DBTMAX': 'no label', 'SLPRT': 'no label', 'SHAPE_Leng': 'no label', });
+lyr_SUNGAI_LN_25K_4.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
